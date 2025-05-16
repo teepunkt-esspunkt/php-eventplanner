e3a488322729d6c27108598b335d5bd3d6dd2e33
Tarek Saleh <tsaleh@web.de>
2025-05-16 21:52:57 +0200

    Delete veranstaltungskalender directory

--- veranstaltungskalender/veranstaltung-loeschen-ok.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'functions.php';
-$ausgabe['titel'] = 'Gelöscht!';
-include TEMPLATES . 'htmlkopf.phtml';
-?>
-
-            <h3><a href="pindexadmin.php">Liste anzeigen</a></h3>
-     <?php
-include TEMPLATES . 'htmlfuss.phtml';
